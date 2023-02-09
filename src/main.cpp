cd9c23897993cb86b28a29a38f69d46b467a5e1f
maturk <maturk@maturks-MBP.home>
2023-02-10 00:32:32 +0100

    circle intersection

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,28 @@
 
 RAYTRACER_NAMESPACE_BEGIN
 
+float hit_sphere(const Point3f& center, double radius, const Ray& r) {
+    Vector3f oc = r.o() - center;
+    auto a = r.w().dot(r.w());
+    auto b = 2.0 * oc.dot(r.w());
+    auto c = oc.dot(oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant) ) / (2.0*a); // smallest t
+    }
+}
+
+
 Color ray_color(const Ray& r) {
+    auto t = hit_sphere(Point3f(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        Vector3f n = (r.at(t) - Vector3f(0,0,-1)).normalized(); // (P - C).norm() = normal at surface
+        return 0.5*Color(n.x()+1, n.y()+1, n.z()+1); // mapping [-1,1] coordinates to [0,1] interval
+    }
     Vector3f unit_direction = (r.w());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0); // y coordinate mapped from [-1,1] to [0,1]
     return (1.0-t)*Color(1.0, 1.0, 1.0) + t*Color(0.2, 0.2, 1.0);
 }
 
@@ -38,10 +57,7 @@ int main(){
         for (int i = 0; i < image_width; ++i) {
             auto u = float(i) / (image_width-1);
             auto v = float(j) / (image_height-1);
-            //std::cout<< u << " "<<v<<std::endl;
-            Ray ray(origin, origin +u*horizontal + v*vertical); // no need to make unit vector  
             
-            //std::cout<<lower_left_corner + u*horizontal + v*vertical - origin<<std::endl;
             Ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
             Color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);         
