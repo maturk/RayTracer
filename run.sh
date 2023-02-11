3a0e486095e256b4fb998dcc30bdaaf4347e406a
maturk <maturk@maturks-MBP.home>
2023-02-11 12:02:47 +0100

    ffmpeg convert

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-'''
-run main program and save render to output file
-'''
+# run main program and save render to output file
 
 
 mkdir -p out
 rm -f out/image.ppm
 ./build/RayTracer >> out/image.ppm
+ffmpeg -i out/image.ppm out/image.jpg
