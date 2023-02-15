912643cfdb311a971c36e91f34b37c2daa07778f
maturk <maturk@maturks-MBP.home>
2023-02-15 20:02:49 +0100

    Apple to glew migrate

--- src/gui.hpp
+++ src/gui.hpp
@@ -15,9 +15,7 @@
 // Mac OS X
 #ifdef __APPLE__
 #define GL_SILENCE_DEPRECATION // Defined before OpenGL and GLUT includes to avoid deprecation messages
-//#include <Carbon/Carbon.h> // Brings in most Apple specific stuff
-#include <OpenGL/gl.h> // Apple OpenGL haders (version depends on OS X SDK version)
-#include <OpenGL/gl3.h> // Required
+#include <Carbon/Carbon.h> // Brings in most Apple specific stuff
 #include <GL/glew.h>
 #define Sleep(x) // Just ignore sleep on Apple
 #endif
