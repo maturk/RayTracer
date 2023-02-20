719d475657134670fceeac756400905f6e8a757d
maturk <maturk@maturks-MBP.home>
2023-02-20 19:28:04 +0200

    render gui per pixel sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "imgui.h"
 #include "common.h"
-#include "color.h"
+#include "Color.h"
 #include "sphere.h"
 #include "hittable_list.h"
 #include "camera.h"
@@ -14,7 +14,7 @@
 
 RAYTRACER_NAMESPACE_BEGIN
 
-Color ray_color(const Ray& r, const Shape& world, int depth) {
+Color ray_Color(const Ray& r, const Shape& world, int depth) {
     if (depth <= 0) {return Color(0,0,0);}
 
     hit_record rec;
@@ -22,7 +22,7 @@ Color ray_color(const Ray& r, const Shape& world, int depth) {
         Ray scattered;
         Color attenuation;
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation.cwiseProduct(ray_color(scattered, world, depth-1));
+            return attenuation.cwiseProduct(ray_Color(scattered, world, depth-1));
         return Color(0,0,0);
     }
 
@@ -38,85 +38,71 @@ int main(){
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
-    const int max_depth = 5;
+    const int samples_per_pixel = 20;
+    const int max_depth = 10;
     const bool gamma = true;
-    
 
     // World
     hittable_list world;
+
     // Materials
     auto material_ground = make_shared<lambertian>(Color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<dielectric>(1.5);
+    auto material_center = make_shared<lambertian>(Color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(Color(0.8, 0.6, 0.2), 1.0);
-    // Hittable objects
+    auto material_right  = make_shared<metal>(Color(0.8, 0.6, 0.2), 0.0);
+
     world.add(make_shared<Sphere>(Point3f( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<Sphere>(Point3f( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(make_shared<Sphere>(Point3f(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<Sphere>(Point3f(-1.0,    0.0, -1.0), -0.45, material_left));
     world.add(make_shared<Sphere>(Point3f( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
-    Camera camera;
+    Camera camera(Point3f(-2,2,1), Point3f(0,0,-1), Vector3f(0,1,0), 20, aspect_ratio);
 
     //GUI and image
-    GUI gui;
+    GUI gui(image_width,image_height);
     Image image(image_width,image_height);
 
-    // Render
-    //gui.start()
-    //...render...
-    //image.update();
-    //image.display();
-    //gui.end
     bool once = true;
     while (gui.open()){
         gui.start(image);
-        image.update();
-        image.display();
         if (image.m_surface.render == true){ 
-            camera.update(image.m_surface);
-            //std::cerr<<"Width "<<image.m_surface.width<<" "<<"Height "<<image.m_surface.height<<std::endl;
-            for (int j = 0; j <= image.m_surface.height-1; j++) {
-                //std::cerr << "\rScanlines remaining: " << image.m_surface.height-j-1 << " " << std::flush;
-                for (int i = 0; i <= image.m_surface.width-1; i++) {
-                    Color pixel_color(0.5, 0.0, 1.0);
-                    auto u = (i + raytracer::random()) / (image.m_surface.width-1);
-                    auto v = (j + raytracer::random()) / (image.m_surface.height-1);
-                    Ray r = camera.get_ray(u, v);
-                    pixel_color += ray_color(r, world, max_depth);
-                    pixel_color = pixel_color;
-                    image.m_surface.pixels[(j*image.m_surface.width +i) +0] = ((255*pixel_color.x()));
-                    image.m_surface.pixels[(j*image.m_surface.width +i) +1] = ((255*pixel_color.y()));
-                    image.m_surface.pixels[(j*image.m_surface.width +i) +2] = ((255*pixel_color.z()));
-                    image.m_surface.pixels[(j*image.m_surface.width +i) +3] = 1;
+            gui.start(image);
+            for (int s = 1; s <= samples_per_pixel; s++) {
+                std::cerr << "\rSamples per pixel remaining: " << samples_per_pixel-(s-1) << ' ' << std::flush;
+                camera.update(image.m_surface); // update camera viewport
+                for (int y = 0; y < image.m_surface.height; y++){
+                    for (int x = 0; x < image.m_surface.width; x++){
+                        auto u = (x + raytracer::random()) / (image.m_surface.width-1);
+                        auto v = (y + raytracer::random()) / (image.m_surface.height-1);
+                        Ray r = camera.get_ray(u, v);
+                        Color pixel_Color = ray_Color(r, world, max_depth);
+                        pixel_Color = pixel_Color.cwiseSqrt(); //Gamma correct
+                        image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +0] = 
+                            ((image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +0])*((s-1)) + 255*pixel_Color.x())/s;
+                        image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +1] =
+                            ((image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +1])*((s-1)) + 255*pixel_Color.y())/s;
+                        image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +2] =
+                            ((image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +2])*((s-1)) + 255*pixel_Color.z())/s;
+                        image.m_surface.pixels[(y*image.m_surface.width +x) * 4 +3] = (float)0;
+                    }
                 }
+                image.update();
+                image.display();
+                 gui.end(image.m_surface);
             }
-            image.m_surface.render = false;
         }
+        std::cerr << "\rFinished rendering: "<< std::flush;
+        image.m_surface.render = false;
         image.update();
         image.display();
         gui.end(image.m_surface);
     }
     image.destroy();
     gui.destroy();
-
-    //std::cout << "P3\n" << image_width <<" "<< image_height <<"\n255\n";
-    //    for (int j = image_height-1; j >= 0; --j) {
-    //        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-    //        for (int i = 0; i < image_width; ++i) {
-    //            Color pixel_color(0, 0, 0);
-    //            for (int s = 0; s < samples_per_pixel; ++s) {
-    //                auto u = (i + raytracer::random()) / (image_width-1);
-    //                auto v = (j + raytracer::random()) / (image_height-1);
-    //                Ray r = camera.get_ray(u, v);
-    //                pixel_color += ray_color(r, world, max_depth);
-    //            }
-    //            write_color(std::cout, pixel_color, samples_per_pixel, gamma); 
-    //        }
-    //    }
 
     std::cerr << "\nDone.\n";
     return 0;
