c0564638d108058753c962327297c88bf7f2dfe9
maturk <maturk@maturks-MBP.home>
2023-02-24 16:49:14 +0200

    ppm to jpg converter

---
+++ convert.sh
@@ -0,0 +1,3 @@
+# convert ppm image to jpg
+
+ffmpeg -i out/image.ppm -vf vflip out/image.jpg   -y 
--- run.sh
+++
@@ -1,7 +0,0 @@
-# run main program and save render to output file
-
-
-mkdir -p out
-rm -f out/image.ppm
-./build/RayTracer >> out/image.ppm
-ffmpeg -i out/image.ppm out/image.jpg -y
